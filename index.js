dcf7cd5412cf7bf04694d71a9c49129365e6e5b0
Your Name <you@example.com>
2019-07-08 08:05:26 +0300

    Add project

--- index.js
+++ index.js
@@ -30,7 +30,12 @@ bot.on('message', (msg) => {
 
     bot.sendMessage(msg.chat.id, jsonFile.welcomeFraze, {
         reply_markup: {
-            keyboard: [[jsonFile.contactsBtn], [jsonFile.goodsBtn],]
+            keyboard: [[jsonFile.contactsBtn], [jsonFile.goodsBtn],],
+            inline_keyboard: [
+                [{ text: 'Кнопка 1', callback_data: '1' }],
+                [{ text: 'Кнопка 2', callback_data: 'data 2' }],
+                [{ text: 'Кнопка 3', callback_data: 'text 3' }]
+              ]
         }
     }
     );
